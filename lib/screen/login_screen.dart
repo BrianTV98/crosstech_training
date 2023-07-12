5a6ccfb0eebbf01c4b7843eb62b139c5ea5d4196
tranhieu <tranhieuglpk@gmail.com>
2023-07-12 21:39:17 +0700

    form login

--- lib/screen/login_screen.dart
+++ lib/screen/login_screen.dart
@@ -10,33 +10,66 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+
+  GlobalKey<FormFieldState> pwStateKey =  GlobalKey<FormFieldState>();
+
   @override
   Widget build(BuildContext context) {
+    final border = OutlineInputBorder(borderRadius: BorderRadius.circular(20));
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children:   [
-            Container(
-              padding: EdgeInsets.symmetric(horizontal: 16),
-              decoration: BoxDecoration(
-                  color: Colors.red,
-                borderRadius: BorderRadius.circular(30)
-              ),
-              child: const Center(
-                child: Text(
-                  'Đăng nhập để học ngay nhé',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontWeight: FontWeight.w700,
-                    fontSize: 18,
-                    height: 3
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 16),
+                decoration: BoxDecoration(
+                    color: Colors.red, borderRadius: BorderRadius.circular(30)),
+                child: const Center(
+                  child: Text(
+                    'Đăng nhập để học ngay nhé',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        fontWeight: FontWeight.w700, fontSize: 18, height: 3),
                   ),
                 ),
               ),
-            ),
-            const TextField()
-          ],
+              const SizedBox(
+                height: 50,
+              ),
+              TextFormField(
+                // key: pwStateKey,
+                decoration: InputDecoration(
+                  border: border,
+                ),
+                validator: (text) {
+                  if (text == null) return '';
+                  if (text.length < 6) {
+                    return 'Mật khẩu quá ngắn';
+                  }
+                },
+              ),
+              const SizedBox(height: 20,),
+              TextFormField(
+                key: pwStateKey,
+                decoration: InputDecoration(
+                  border: border,
+                ),
+                validator: (text) {
+                  if (text == null) return '';
+                  if (text.length < 6) {
+                    return 'Mật khẩu quá ngắn';
+                  }
+                },
+              ),
+              const ElevatedButton(
+                onPressed:null,//(){},
+                child: Text('Đăng nhập'),
+              )
+            ],
+          ),
         ),
       ),
     );
